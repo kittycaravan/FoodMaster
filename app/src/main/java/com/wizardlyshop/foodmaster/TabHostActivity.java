0cd13812b579fba1c4ab4a6017d45c239837e0d1
kittycaravan <kittycaravan@gmail.com>
2015-05-05 01:56:38 +0900

    creator 삭제 #8

--- app/src/main/java/com/wizardlyshop/foodmaster/TabHostActivity.java
+++ app/src/main/java/com/wizardlyshop/foodmaster/TabHostActivity.java
@@ -1,5 +1,5 @@
 package com.wizardlyshop.foodmaster;
-import com.wizardlyshop.foodmaster.R;
+
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,33 +9,28 @@ import android.widget.TabHost.TabSpec;
 
 public class TabHostActivity extends TabActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.tabhost);
-		
-		TabHost tabHost=getTabHost();
-		TabSpec tabSpec=tabHost.newTabSpec("tab1");
-		Intent intent=new Intent(TabHostActivity.this, SelectActivity.class);
-		tabSpec.setIndicator("Select").setContent(intent);
-		tabHost.addTab(tabSpec);
-		
-		tabSpec=tabHost.newTabSpec("tab2");
-		intent=new Intent(TabHostActivity.this, FavoritesActivity.class);
-		tabSpec.setIndicator("Favorites").setContent(intent);
-		tabHost.addTab(tabSpec);
-		
-		tabSpec=tabHost.newTabSpec("tab4");
-		intent=new Intent(TabHostActivity.this, CreatorActivity.class);
-		tabSpec.setIndicator("Creator").setContent(intent);
-		tabHost.addTab(tabSpec);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tabhost);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        TabHost tabHost = getTabHost();
+        TabSpec tabSpec = tabHost.newTabSpec("tab1");
+        Intent intent = new Intent(TabHostActivity.this, SelectActivity.class);
+        tabSpec.setIndicator("Select").setContent(intent);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tab2");
+        intent = new Intent(TabHostActivity.this, FavoritesActivity.class);
+        tabSpec.setIndicator("Favorites").setContent(intent);
+        tabHost.addTab(tabSpec);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
