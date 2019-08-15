51be916a0f7776e0f15d38d8b35d1bfbad38c9d8
klesa <kittycaravan@gmail.com>
2019-08-15 17:37:25 +0900

    스플래시에서 메인으로 3초 후에 자동으로 넘어가기

--- app/src/main/java/com/wizardlyshop/foodmaster/ActivitySplash.java
+++ app/src/main/java/com/wizardlyshop/foodmaster/ActivitySplash.java
@@ -5,26 +5,28 @@ import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 public class ActivitySplash extends AppCompatActivity {
+	private static final int DELAY_MILLI_SEC = 3000;
 	private MediaPlayer mMediaPlayer;
-
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         playIntroVoice();
-        Button btn = findViewById(R.id.buttonStart);
-        btn.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() { //여기에 딜레이 후 시작할 작업들을 입력
 				Intent intent;
 				intent=new Intent(getApplicationContext(), ActivityTabHost.class);
 				startActivity(intent);
 				finish();
 			}
-		});
+		}, DELAY_MILLI_SEC); // n (ms)초 정도 딜레이를 준 후 시작
     }
     private void playIntroVoice(){
 		killMediaPlayer();
@@ -34,16 +36,14 @@ public class ActivitySplash extends AppCompatActivity {
 			mMediaPlayer.prepare();
 		} catch (Exception e){}
 		mMediaPlayer.start();
-    }
-
-    private void killMediaPlayer(){
+	}
+	private void killMediaPlayer() {
 		if(mMediaPlayer != null){
 			try{
 				mMediaPlayer.release();
 			} catch (Exception e) { }
 		}
 	}
-
 	@Override
 	protected void onDestroy() {
     	killMediaPlayer();
